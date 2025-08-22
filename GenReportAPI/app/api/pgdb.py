7915f84d3110c5870fc31733c010d365484c40f0
tuanna712 <tuanna712@gmail.com>
2025-08-21 21:54:30 -0600

    update gas report api

--- GenReportAPI/app/api/pgdb.py
+++ GenReportAPI/app/api/pgdb.py
@@ -2,7 +2,7 @@ import psycopg2
 import pandas as pd
 from datetime import datetime
 
-class PGDatabase:
+class PGOilQuery:
     def __init__(self, dbname, user, password, host, port):
         self.conn = psycopg2.connect(
             host=host,
@@ -191,13 +191,13 @@ class PGDatabase:
     
 
 
-def generate_report(query_date, 
+def generate_oil_report(query_date, 
                     POSTGRES_DB, 
                     POSTGRES_USER, 
                     POSTGRES_PASSWORD,
                     HOST,
                     PORT):
-    PGDB = PGDatabase(
+    PGDB = PGOilQuery(
         dbname=POSTGRES_DB,
         user=POSTGRES_USER,
         password=POSTGRES_PASSWORD,
@@ -361,3 +361,289 @@ def generate_report(query_date,
     return pd.DataFrame(data)
 
 
+
+class PGGasQuery:
+    def __init__(self, dbname, user, password, host, port):
+        self.conn = psycopg2.connect(
+            host=host,
+            port=port,
+            dbname=dbname,
+            user=user,
+            password=password
+        )
+        self.cur = self.conn.cursor()
+    
+    def get_all_table_names(self):
+        self.cur.execute("""
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public';
+        """)
+        return [row[0] for row in self.cur.fetchall()]
+    
+    #=======GET FOR REPORTING=========
+    # Column C, D
+    def get_accum_plan_year(self, field_id, year, plan_type):
+        # Extract the accumulated production for a specific year by field_id
+        self.cur.execute("""
+            SELECT SUM(prod_m3) FROM plan_prod
+            WHERE field_id = %s AND EXTRACT(YEAR FROM report_date) = %s AND plan_type = %s;
+        """, (field_id, year, plan_type))
+        return self.cur.fetchone()[0]
+    
+    def get_data_by_field(self, field_id):
+        """Get datetime and prod_m3 by field_id and prod_type='GAS_PROD'"""
+        self.cur.execute("""
+            SELECT report_date, prod_m3 FROM daily_prod
+            WHERE field_id = %s AND prod_type = 'GAS_PROD';
+        """, (field_id,))
+        return self.cur.fetchall()
+
+    def get_all_production_data(self):
+        """Get all production data for all fields and prod_type='GAS_PROD'"""
+        self.cur.execute("""
+            SELECT field_id, report_date, prod_m3 FROM daily_prod
+            WHERE prod_type = 'GAS_PROD';
+        """)
+        return self.cur.fetchall()
+    
+    # Column D
+    def get_monthly_prod(self, field_id, month, prod_type, year=2025):
+        # Extract the production for a specific month by field_id
+        self.cur.execute("""
+            SELECT SUM(prod_m3) FROM daily_prod
+            WHERE field_id = %s AND EXTRACT(MONTH FROM report_date) = %s AND EXTRACT(YEAR FROM report_date) = %s AND prod_type = %s;
+        """, (field_id, month, year, prod_type))
+        return self.cur.fetchone()[0]
+    
+    def get_accum_daily(self, field_id, month, prod_type, year=2025):
+        # Extract the accumulated production from Jan to a specific month by field_id
+        if month < 1 or month > 12:
+            print("Invalid month. Month must be between 1 and 12.")
+            return None
+        if month == 1:
+            return 0
+        accum_prod = 0
+        for i in range(1, month):
+            monthly_prod = self.get_monthly_prod(field_id, i, prod_type, year)
+            if monthly_prod is not None:
+                accum_prod += monthly_prod
+        return accum_prod
+    
+    # Column H
+    def get_monthly_plan_prod(self, field_id, month, plan_type, year=2025):
+        # Extract the production for a specific month by field_id
+        self.cur.execute("""
+            SELECT SUM(prod_m3) FROM plan_prod
+            WHERE field_id = %s AND EXTRACT(MONTH FROM report_date) = %s AND EXTRACT(YEAR FROM report_date) = %s AND plan_type = %s;
+        """, (field_id, month, year, plan_type))
+        return self.cur.fetchone()[0]
+    
+    # Column J
+    def get_accum_monthly_prod_to_a_date(self, field_id, report_date, prod_type):
+        # report_date in yyyy/mm/dd
+        # Extract the accumulated production from 1/1 to the specified date by field_id
+        self.cur.execute("""
+            SELECT SUM(prod_m3) FROM daily_prod
+            WHERE field_id = %s AND report_date BETWEEN %s AND %s AND prod_type = %s;
+        """, (field_id, f'{report_date.year}-{report_date.month}-01', report_date, prod_type))
+        return self.cur.fetchone()[0]
+    
+    # Column N
+    def get_accum_daily_prod_up_to_date(self, field_id, report_date, prod_type, unit, year):
+        # Extract accumulated prod up to date from 1/1/year
+        self.cur.execute(f"""
+            SELECT SUM({unit}) FROM daily_prod
+            WHERE field_id = %s AND report_date BETWEEN '{year}-01-01' AND %s AND prod_type = %s;
+        """, (field_id, report_date, prod_type))
+        return self.cur.fetchone()[0]
+    
+    # Column Q, R
+    def get_daily_prod_by_date(self, field_id, report_date, prod_type, unit='prod_bbls'):
+        self.cur.execute(f"""
+            SELECT {unit} FROM daily_prod
+            WHERE field_id = %s AND report_date = %s AND prod_type = %s;
+        """, (field_id, report_date, prod_type))
+        return self.cur.fetchone()
+    
+def generate_gas_report(query_date, 
+                    POSTGRES_DB, 
+                    POSTGRES_USER, 
+                    POSTGRES_PASSWORD,
+                    HOST,
+                    PORT):
+    PGDB = PGGasQuery(
+        dbname=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=HOST,
+        port=PORT
+    )
+
+    query_date = datetime.strptime(query_date, "%Y/%m/%d")
+    month = query_date.month
+    year = query_date.year
+    day = query_date.day
+
+    # Column B
+    field_names = [
+        'Bạch Hổ+ Rồng',
+        'Tê Giác Trắng',
+        'Rạng Đông+Phương Đông',
+        'Chim Sáo+  Dừa',
+        'STĐ+STV+STT+STN',
+        'Cá Ngừ Vàng',
+        'Lan Tây+Lan Đỏ',
+        'Rồng Đôi+Rồng Đôi Tây',
+        'Lô PM3-CAA ( tổng khí về bờ)',
+        'Hải Sư Trắng +Hải Sư Đen',
+        'Hải Thạch + Mộc Tinh',
+        'Thái Bình',
+        'Thiên Ưng',
+        'Sao Vàng -Đại Nguyet',
+        'Đại Hùng',
+        'Cá Tầm',
+    ]
+    KHQT_fields = ['BH', 'TGT', 'RangDong', 'CS', 'STD-STV-STT', 'CNV', 'LTLD', 'RD-RDT', 'PM3CA-46CN', 'HST-HSD', 'HT-MT', 'TB', 'ThienUng', 'SVDN', 'DH', 'CT']
+    KHCP_fields = ['BH', 'TGT', 'RDPD', 'CS-D', 'STD-STV-STT-STN', 'CNV', 'LTLD', 'RD-RDT', 'PM3CA-46CN', 'HST-HSD', 'HT-MT', 'TB', 'ThienUng', 'SVDN', 'DH', 'CT']
+
+    # Column C
+    column_c = [PGDB.get_accum_plan_year(field_id=field, year=year, plan_type='KHSLCPGiaoGas') for field in KHCP_fields]
+    # Column D
+    column_d = [PGDB.get_accum_plan_year(field_id=field, year=year, plan_type='KHQTGAS') for field in KHQT_fields]
+
+    # Column E
+    sub_field_ids = [('BH', 'R'), 
+                    'TGT',
+                    ('RangDong', 'PhuongDong'),
+                    'CS',
+                    ('STD', 'STV', 'STD-DB', 'STT', 'STN'), 
+                    'CNV', 'LT', 'RD-RDT', 
+                    'PM3-46CN',
+                    'HST-HSD', 'HT', 'ThaiBinh', 'ThienUng', 'SV', 'DH', 'CT'
+                    ]
+    column_e = []
+    for _field in sub_field_ids:
+        if isinstance(_field, tuple):
+            _sub_field_prod = []
+            for sub_field in _field:
+                _sub_field_prod.append(PGDB.get_accum_daily(field_id=sub_field, month=month, prod_type='GAS_PROD'))
+            column_e.append(sum(_sub_field_prod))
+        else:
+            column_e.append(PGDB.get_accum_daily(field_id=_field, month=month, prod_type='GAS_PROD'))
+
+    # Column F
+    column_f = [e * 100 / (c) if c != 0 else 0 for e, c in zip(column_e, column_c)]
+    # Column G
+    column_g = [e * 100 / (d) if d != 0 else 0 for e, d in zip(column_e, column_d)]
+    # Column H
+    column_h = [PGDB.get_monthly_plan_prod(field_id=field, month=month, plan_type='KHSLCPGiaoGas') for field in KHCP_fields]
+    # Column I
+    column_i = [PGDB.get_monthly_plan_prod(field_id=field, month=month, plan_type='KHQTGAS') for field in KHQT_fields]
+    # Column J
+    column_j = []
+    for _field in sub_field_ids:
+        if isinstance(_field, tuple):
+            _sub_field_prod = []
+            for sub_field in _field:
+                try:
+                    _prod = PGDB.get_accum_monthly_prod_to_a_date(field_id=sub_field, report_date=query_date, prod_type='GAS_PROD')
+                    if _prod is None:
+                        _prod = 0
+                    _sub_field_prod.append(_prod)
+                except Exception as e:
+                    _sub_field_prod.append(0)
+            column_j.append(sum(_sub_field_prod))
+        else:
+            column_j.append(PGDB.get_accum_monthly_prod_to_a_date(field_id=_field, report_date=query_date, prod_type='GAS_PROD'))
+    # # Column K
+    column_k = [j * 100 / h if h != 0 else 0 for j, h in zip(column_j, column_h)]
+
+    # Column L
+    column_l = [j * 100 / i if i != 0 else 0 for j, i in zip(column_j, column_i)]
+
+    # Column M
+    column_m = [e + j for e, j in zip(column_e, column_j)]
+
+    # Column N
+    column_n = []
+    for _field in sub_field_ids:
+        if isinstance(_field, tuple):
+            _unused_fields = ['Pearl', 'Topaz', 'Diamond', 'HSD']
+            _v = 0
+            for sub_field in _field:
+                if sub_field in _unused_fields:
+                    _v += 0
+                else:
+                    _prod = PGDB.get_accum_daily_prod_up_to_date(field_id=sub_field, report_date=query_date, prod_type='GAS_PROD', unit='prod_ft3', year=year)
+                    if _prod is None:
+                        _prod = 0
+                    _v += _prod
+            column_n.append(_v)
+        else:
+            column_n.append(PGDB.get_accum_daily_prod_up_to_date(field_id=_field, report_date=query_date, prod_type='GAS_PROD', unit='prod_ft3', year=year))
+
+    # Column O
+    column_o = [100*m/c if c != 0 else 0 for m, c in zip(column_m, column_c)]
+
+    # Column P
+    column_p = [100*m/d if d != 0 else 0 for m, d in zip(column_m, column_d)]
+
+    # Column Q
+    column_q = []
+    for _field in sub_field_ids:
+        if isinstance(_field, tuple):
+            _v = 0
+            for sub_field in _field:
+                _prod = PGDB.get_daily_prod_by_date(field_id=sub_field, report_date=query_date, prod_type='GAS_PROD', unit='prod_m3')
+                if _prod is None:
+                    _prod = 0
+                    _v += _prod
+                else:
+                    _v += _prod[0]
+            column_q.append(_v)
+        else:
+            _prod = PGDB.get_daily_prod_by_date(field_id=_field, report_date=query_date, prod_type='GAS_PROD', unit='prod_m3')
+            column_q.append(_prod[0])
+
+    # Column R
+    column_r = []
+    for _field in sub_field_ids:
+        if isinstance(_field, tuple):
+            _unused_fields = ['Pearl', 'Topaz', 'Diamond', 'HSD']
+            _v = 0
+            for sub_field in _field:
+                if sub_field in _unused_fields:
+                    _v +=0
+                else:
+                    _prod = PGDB.get_daily_prod_by_date(field_id=sub_field, report_date=query_date, prod_type='GAS_PROD', unit='prod_ft3')
+                    if _prod is None:
+                        _prod = 0
+                        _v += _prod
+                    else:
+                        _v += _prod[0]
+            column_r.append(_v)
+        else:
+            _prod = PGDB.get_daily_prod_by_date(field_id=_field, report_date=query_date, prod_type='GAS_PROD', unit='prod_ft3')
+            column_r.append(_prod[0])
+            
+    data = {
+            "Mỏ": field_names,
+            "KHCP  (tr.m3)": ["%.2f" % elem for elem in column_c],
+            "KHQT  (tr.m3)": ["%.2f" % elem for elem in column_d],
+            "Tháng trước - Cộng dồn (tr.m3)": ["%.2f" % elem for elem in column_e],
+            "Tháng trước - %KHCP": ["%.2f" % elem for elem in column_f],
+            "Tháng trước - %KHQT": ["%.2f" % elem for elem in column_g],
+            "Tháng này - KHCP (tr.m3)": ["%.2f" % elem for elem in column_h],
+            "Tháng này - KHQT (tr.m3)": ["%.2f" % elem for elem in column_i],
+            "Tháng này - Thực hiện (tr.m3)": ["%.2f" % elem for elem in column_j],
+            "Tháng này - %KHCP": ["%.2f" % elem for elem in column_k],
+            "Tháng này - %KHQT": ["%.2f" % elem for elem in column_l],
+            "SL hiện tại - Cộng dồn (tr.m3)": ["%.2f" % elem for elem in column_m],
+            "SL hiện tại - Cộng dồn (tr.ft3)": ["%.2f" % elem for elem in column_n],
+            "SL hiện tại - %KHCP": ["%.2f" % elem for elem in column_o],
+            "SL hiện tại - %KHQT": ["%.2f" % elem for elem in column_p],
+            "SL ngày (tr.m3)": ["%.2f" % elem for elem in column_q],
+            "SL ngày (tr.ft3)": ["%.2f" % elem for elem in column_r]
+        }
+    return pd.DataFrame(data)
--- GenReportAPI/app/api/pgsql.py
+++ GenReportAPI/app/api/pgsql.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .pgdb import generate_report
+from .pgdb import generate_oil_report, generate_gas_report
 from fastapi.responses import StreamingResponse, JSONResponse
 import pandas as pd
 from io import StringIO
@@ -7,12 +7,9 @@ from app.schemas.pgsql import ReportRequest
 
 router = APIRouter()
 
-@router.post("/df")
-def gen_df_report(request: ReportRequest):
-    """
-    Return dataframe as JSON
-    """
-    report_df: pd.DataFrame = generate_report(
+@router.post("/oilreport")
+def gen_df_oil_report(request: ReportRequest):
+    report_df: pd.DataFrame = generate_oil_report(
         request.query_date,
         request.POSTGRES_DB,
         request.POSTGRES_USER,
@@ -22,12 +19,9 @@ def gen_df_report(request: ReportRequest):
     )
     return JSONResponse(content=report_df.to_dict(orient="records"))
 
-@router.post("/csv")
-def gen_csv_report(request: ReportRequest):
-    """
-    Return dataframe as CSV file
-    """
-    report_df: pd.DataFrame = generate_report(
+@router.post("/gasreport")
+def gen_df_gas_report(request: ReportRequest):
+    report_df: pd.DataFrame = generate_gas_report(
         request.query_date,
         request.POSTGRES_DB,
         request.POSTGRES_USER,
@@ -35,12 +29,27 @@ def gen_csv_report(request: ReportRequest):
         request.HOST,
         request.PORT
     )
+    return JSONResponse(content=report_df.to_dict(orient="records"))
 
-    stream = StringIO()
-    report_df.to_csv(stream, index=False)
-    response = StreamingResponse(
-        iter([stream.getvalue()]),
-        media_type="text/csv"
-    )
-    response.headers["Content-Disposition"] = "attachment; filename=daily_oil_report.csv"
-    return response
+# @router.post("/oilcsv")
+# def gen_csv_report(request: ReportRequest):
+#     """
+#     Return dataframe as CSV file
+#     """
+#     report_df: pd.DataFrame = generate_oil_report(
+#         request.query_date,
+#         request.POSTGRES_DB,
+#         request.POSTGRES_USER,
+#         request.POSTGRES_PASSWORD,
+#         request.HOST,
+#         request.PORT
+#     )
+
+#     stream = StringIO()
+#     report_df.to_csv(stream, index=False)
+#     response = StreamingResponse(
+#         iter([stream.getvalue()]),
+#         media_type="text/csv"
+#     )
+#     response.headers["Content-Disposition"] = "attachment; filename=daily_oil_report.csv"
+#     return response
